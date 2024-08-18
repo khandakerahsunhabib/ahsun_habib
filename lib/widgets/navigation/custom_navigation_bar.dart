3636024ea401018707f3cef79ba31f745784d476
Khandakerahsunhabib <ahsunhabib96bd@gmail.com>
2024-08-18 21:01:49 +0600

    Navigation updated and Custom theme added

--- lib/widgets/navigation/custom_navigation_bar.dart
+++ lib/widgets/navigation/custom_navigation_bar.dart
@@ -1,49 +1,59 @@
+import 'package:ahsun_habib/widgets/navigation/navbar_item.dart';
+import 'package:ahsun_habib/widgets/navigation/navbar_logo.dart';
 import 'package:flutter/material.dart';
-import 'navbar_item.dart';
-import 'navbar_logo.dart';
 
 class CustomNavigationBar extends StatelessWidget {
   const CustomNavigationBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          NavbarLogo(),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              NavbarItem(title: 'Home'),
-              SizedBox(
-                width: 30,
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const NavbarLogo(),
+        const Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            NavbarItem(title: 'Home'),
+            SizedBox(
+              width: 30,
+            ),
+            NavbarItem(title: 'Skills'),
+            SizedBox(
+              width: 30,
+            ),
+            NavbarItem(title: 'Projects'),
+            SizedBox(
+              width: 30,
+            ),
+            NavbarItem(title: 'Blog'),
+            SizedBox(
+              width: 30,
+            ),
+            NavbarItem(title: 'About'),
+          ],
+        ),
+        SizedBox(
+          height: 40,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(3),
               ),
-              NavbarItem(title: 'About'),
-              SizedBox(
-                width: 30,
-              ),
-              NavbarItem(title: 'Services'),
-              SizedBox(
-                width: 30,
-              ),
-              NavbarItem(title: 'Education'),
-              SizedBox(
-                width: 30,
-              ),
-              NavbarItem(title: 'Portfolio'),
-              SizedBox(
-                width: 30,
-              ),
-              NavbarItem(title: 'Blog'),
-              SizedBox(
-                width: 30,
-              ),
-              NavbarItem(title: 'Contact'),
-            ],
-          )
-        ],
-      ),
+            ),
+            onPressed: () {
+              print('You clicked on the button');
+            },
+            child: Text(
+              'Contact Me',
+              style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                    fontSize: 18,
+                  ),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
